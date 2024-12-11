435b9cc342d0db6e99eb607989c10e0a616910d1
elbachir-salik <salik.elbachir@gmail.com>
2024-12-11 15:08:54 +0100

    Add Event model with fields for event management

--- events/models.py
+++ events/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=255)
+    is_approved = models.BooleanField(default=False)
+    organizer = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="events"
+    )
+
+    def __str__(self):
+        return self.name
