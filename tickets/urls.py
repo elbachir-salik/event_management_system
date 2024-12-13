13f42a52bdf930b32e775a224ce7544f71d04558
elbachir-salik <salik.elbachir@gmail.com>
2024-12-13 09:39:18 +0100

    Cancel ticket endpoint

--- tickets/urls.py
+++ tickets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import BookTicketView
+from .views import BookTicketView, CancelTicketView
 
 urlpatterns = [
     path('book/<int:event_id>/', BookTicketView.as_view(), name='book_ticket'),
+    path('cancel/<int:ticket_id>/', CancelTicketView.as_view(), name='cancel_ticket'),
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -28,3 +28,17 @@ class BookTicketView(APIView):
         return Response({'message':f'Ticket booked for event "{event.name}".'}, status=status.HTTP_201_CREATED)
     
 
+class CancelTicketView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request, ticket_id):
+        try:
+            ticket = Ticket.objects.get(id= ticket_id, user=request.user)
+        
+        except Ticket.DoesNotExist:
+            return Response({'error':'Ticket Not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        event_name = ticket.event.name
+        ticket.delete()
+        return Response({'message':f'Your ticket for the event "{event_name}" has been canceled'}, status=status.HTTP_200_OK)
+    
